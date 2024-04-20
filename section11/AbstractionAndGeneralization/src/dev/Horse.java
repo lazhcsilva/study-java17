514fa2c7a46f16d5388ddf16a8fd1cbcf651609b
Lázaro Silva <lazhcsilva@gmail.com>
2024-04-19 21:11:54 -0300

    Abstract Class

---
+++ section11/AbstractionAndGeneralization/src/dev/Horse.java
@@ -0,0 +1,18 @@
+package dev;
+
+public class Horse extends Mammal {
+
+    public Horse(String type, String size, double wight) {
+        super(type, size, wight);
+    }
+
+    @Override
+    public void shedHair() {
+        System.out.println(getExplicitType() + " sheds in the spring");
+    }
+
+    @Override
+    public void makeNoise() {
+
+    }
+}
